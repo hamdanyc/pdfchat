60b5ff857eea2f65df2f2547d1d349ba48fc99ed
Hamdan Yaccob Kol (Rtd) <113873149+hamdanyc@users.noreply.github.com>
2024-02-03 11:36:16 +0800

    Create chatfdr_app.py

---
+++ chatfdr_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import requests
+import os
+
+# Function to make the API request with a custom question
+def make_api_request(question):
+    url = 'http://documind.onrender.com/api-ask-from-collection'
+    files = {
+        'secretkey': (None, 'your_api_key'),
+        'question': (None, question),
+        'folder_id': (None, 'a62cadb4-5b79-4090-8097-06d546a0f0d5'),
+        'enable_gpt4': (None, 'false')
+    }
+
+    response = requests.post(url, files=files)
+    return response.text
+
+# Streamlit app code
+st.title('Chat with Minutes of EXCO Meeting')
+
+# Add a text input for the user to enter the question
+user_question = st.text_input('Enter your query')
+
+# Add a button to trigger the API request
+if st.button('Ask Question'):
+    if user_question:
+        response_text = make_api_request(user_question)
+        st.write('API Response:')
+        st.code(response_text)
+    else:
+        st.write('Please enter a question')
