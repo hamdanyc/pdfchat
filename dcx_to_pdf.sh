4f127d72f25ca29505648cfae4ee882a5f7ae7c4
hamdanyc <hamdan.hy@gmail.com>
2024-02-05 12:56:47 +0800

    path to unoconv and basename

--- dcx_to_pdf.sh
+++ dcx_to_pdf.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Iterate through each folder (feb, mar, apr, etc.)
-for folder in feb mar apr ; do
+for folder in feb; do
     # Create pdf sub-folder if it doesn't exist
     mkdir -p $folder/pdf
     
     # Iterate through each docx file in the current folder
     for file in $folder/*.docx ; do
         # Get the filename without extension
-        filename=$(basename $file .docx)
+        filename=$(basename "$file" .docx)
         
         # Convert docx to pdf using unoconv cli
-        unoconv -f pdf -o $folder/pdf $file
+        /usr/bin/unoconv -f pdf -o $folder/pdf "$file"
     done
 done
